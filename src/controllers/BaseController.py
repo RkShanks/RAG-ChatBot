03cd3e3cd1fe478e3b9623c3deb30c13ebe08e93
RkShanks <zid.omar128885@gmail.com>
2026-03-16 05:12:26 +0200

    feat: add get_database_path func to get database path and create it if it's not exists

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -14,8 +14,23 @@ class BaseController:
             "assets",
             "files",
         )
+        self.database_dir = os.path.join(
+            self.main_dir,
+            "assets",
+            "local_database",
+        )
 
     def generate_random_string(self, length: int = 12):
         # Generate a random string of fixed length.
         letters = string.ascii_lowercase + string.digits
         return "".join(random.choice(letters) for _ in range(length))
+
+    def get_database_path(self, database_name: str):
+        database_path = os.path.join(
+            self.database_dir,
+            database_name,
+        )
+
+        if not os.path.exists(database_path):
+            os.makedirs(database_path)
+        return database_path
