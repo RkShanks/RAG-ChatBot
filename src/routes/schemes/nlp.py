8fe11124e278e87f88e6d09eb3201c9bc2a98b7c
RkShanks <zid.omar128885@gmail.com>
2026-04-08 01:20:13 +0200

    feat(nlp): add target_locale and chat_history fields to SearchRequest model

--- src/routes/schemes/nlp.py
+++ src/routes/schemes/nlp.py
@@ -7,3 +7,5 @@ class SearchRequest(BaseModel):
     query: str
     limit: Optional[int] = 5
     filter_criteria: Optional[dict] = None
+    target_locale: Optional[str] = "based in last user Query"
+    chat_history: Optional[list] = None
